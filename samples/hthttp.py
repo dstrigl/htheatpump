b8ba45d1d71aee4a3021397c6ef7d33f807d7da3
dstrigl <strigl.daniel@gmail.com>
2019-01-04 17:20:51 +0100

    Ongoing work on the hthttp sample.

--- samples/hthttp.py
+++ samples/hthttp.py
@@ -1,69 +1,78 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#  htheatpump - Serial communication module for Heliotherm heat pumps
+#  Copyright (C) 2019  Daniel Strigl
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib import parse as urlparse
 from htheatpump.htheatpump import HtHeatpump
 from htheatpump.htparams import HtParams
+import json
+import logging
 
 
 class GetHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
-        message_parts = [
-            'CLIENT VALUES:',
-            'client_address=%s (%s)' % (self.client_address, self.address_string()),
-            'command=%s' % self.command,
-            'path=%s' % self.path,
-            'real path=%s' % parsed_path.path,
-            'query=%s' % parsed_path.query,
-            'request_version=%s' % self.request_version,
-            '',
-            'SERVER VALUES:',
-            'server_version=%s' % self.server_version,
-            'sys_version=%s' % self.sys_version,
-            'protocol_version=%s' % self.protocol_version,
-            '',
-            'HEADERS RECEIVED:',
-        ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append('%s=%s' % (name, value.rstrip()))
-        message_parts.append('')
+        qsl = urlparse.parse_qsl(parsed_path.query, keep_blank_values=True)
+        print(qsl)
 
-        qsl = urlparse.parse_qsl(parsed_path.query)
-        param_parts = []
+        hp.login()
+        params = {}
         for query in qsl:
             name, value = query
-            # convert the passed value (as string) to the specific data type
-            value = HtParams[name].from_str(value)
-            # set the parameter of the heat pump to the passed value
-            value = hp.set_param(name, value)
-            param_parts.append('{} = {}'.format(name, value))
-            print('{} = {}'.format(name, value))
-
+            if not value:
+                # query for the given parameter
+                value = hp.get_param(name)
+            else:
+                # convert the passed value (as string) to the specific data type
+                value = HtParams[name].from_str(value)
+                # set the parameter of the heat pump to the passed value
+                value = hp.set_param(name, value)
+            params.update({name: value})
+            print("{}: {}".format(name, value))
+        hp.logout()
+ 
         self.send_response(200)
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        message = '\r\n'.join(message_parts) + '\r\n' + '\r\n'.join(param_parts)
-        self.wfile.write(bytes(message, 'utf8'))
+
+        message = json.dumps(params, indent=2, sort_keys=True)
+        self.wfile.write(bytes(message, "utf8"))
         return
 
 
 # Main program
 def main():
+    #logging.basicConfig(level=logging.INFO)
     global hp
-    hp = HtHeatpump('/dev/ttyUSB0', baudrate=115200)
+    hp = HtHeatpump("/dev/ttyUSB0", baudrate=115200)
     hp.open_connection()
     hp.login()
     rid = hp.get_serial_number()
-    print("connected successfully to heat pump with serial number {:d}".format(rid))
+    print("Connected successfully to heat pump with serial number: {:d}".format(rid))
     ver = hp.get_version()
-    print("software version = {} ({:d})".format(ver[0], ver[1]))
-    server = HTTPServer(('192.168.11.90', 8080), GetHandler)
+    print("Software version: {} ({:d})".format(ver[0], ver[1]))
+    hp.logout()
+    server = HTTPServer(("192.168.11.90", 8080), GetHandler)
     print('Starting server at http://localhost:8080, use <Ctrl-C> to stop.')
     server.serve_forever()
-    #hp.logout()  # try to logout for an ordinary cancellation (if possible)
-    #hp.close_connection()
+    hp.close_connection()
 
 
 if __name__ == "__main__":
