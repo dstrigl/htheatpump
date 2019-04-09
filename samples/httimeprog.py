e695b26d41e582b68483dcd35be177dc1ba9cd1b
dast <my@mail.com>
2019-04-09 21:49:01 +0200

    ongoing work

--- samples/httimeprog.py
+++ samples/httimeprog.py
@@ -197,8 +197,8 @@ def main():
                             writer.writerow({"day"  : day,
                                              "entry": entry,
                                              "state": data["state"],
-                                             "begin": ":".join(data["begin"]),
-                                             "end"  : ":".join(data["end"]),
+                                             "begin": "{:02d}:{:02d}".format(*data["begin"]),
+                                             "end"  : "{:02d}:{:02d}".format(*data["end"]),
                                              })
 
         # print execution time only if desired
