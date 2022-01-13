8177d376b36b95cd75fbec4995b904b69fd61c38
dstrigl <577250+dstrigl@users.noreply.github.com>
2022-01-13 11:31:25 +0100

    Updated copyright statements

--- tests/test_htheatpump.py
+++ tests/test_htheatpump.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #  htheatpump - Serial communication module for Heliotherm heat pumps
-#  Copyright (C) 2021  Daniel Strigl
+#  Copyright (C) 2022  Daniel Strigl
 
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
